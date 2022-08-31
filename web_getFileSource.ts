28da690e546b5ea0fc7973cdc61b5429c2d6657a
ck996 <1164537025@qq.com>
2022-08-31 13:50:59 +0800

    文件选择

--- web_getFileSource.ts
+++ web_getFileSource.ts
@@ -1,10 +1,26 @@
-export function web_getFileSource(){
-  const fileInput = document.createElement('input')
-  fileInput.accept = '.docx'
-  fileInput.type = 'file'
-  fileInput.onchange = function (val :Event){
-    const fileReader = new FileReader()
-    console.log(val)
-  }
-  fileInput.click()
+const GET_FILE_SOURCE = {
+  accept: ['*'],
+}
+
+export function web_getFileSource(config = {}){
+  const rConfig = Object.assign({}, GET_FILE_SOURCE, config)
+  return new Promise((resolve, reject) => {
+    const fileInput = document.createElement('input')
+    fileInput.accept = rConfig.accept.join(',')
+    fileInput.type = 'file'
+    fileInput.onchange = function (val){
+      const fileReader = new FileReader()
+      const file = fileInput.files.item(0)
+      if (!file){
+        reject('未选择文件')
+        return;
+      }
+      fileReader.readAsDataURL(file)
+      fileReader.onload = function (){
+        resolve({text: fileReader.result, file: file})
+      }
+      return;
+    }
+    fileInput.click()
+  })
 }
